fd1008c3b494c7699da4c667b8e6da4698cc5e8c
Luqman Satrio Wicaksono <satrio.luqman@gmail.com>
2017-05-31 20:54:41 +0200

    visual polish

--- MediaPlayer_Basic_Usage/app/src/main/java/com/example/luqmansatriowicaksono/mediaplayer_basic_usage/MainActivity.java
+++ MediaPlayer_Basic_Usage/app/src/main/java/com/example/luqmansatriowicaksono/mediaplayer_basic_usage/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.luqmansatriowicaksono.mediaplayer_basic_usage;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView woof1;
+    ImageView woof2;
     MediaPlayer player;
     MediaPlayer wooof;
     TextView woof;
@@ -18,7 +24,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        woof1 = (ImageView) findViewById(R.id.woff);
+        woof2 = (ImageView) findViewById(R.id.woff1);
 
         TextView play = (TextView) findViewById(R.id.play);
         TextView pause = (TextView) findViewById(R.id.pause);
@@ -32,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 player.start();
+                woof1.setImageResource(R.drawable.wuff);
+                woof2.setImageResource(R.drawable.wuff);
+                woof1.setVisibility(View.VISIBLE);
+                woof2.setVisibility(View.VISIBLE);
+
             }
         });
 
@@ -39,15 +54,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 player.pause();
+                woof1.setVisibility(View.INVISIBLE);
+                woof2.setVisibility(View.INVISIBLE);
             }
         });
 
+        final VideoView videoview = (VideoView) findViewById(R.id.video);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.bark);
+        videoview.setVideoURI(uri);
 
 
         pausewoof = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wooof.pause();
+
+                videoview.pause();
                 woof.setOnClickListener(wooflistener);
             }
         };
@@ -58,10 +79,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                ImageView woff = (ImageView) findViewById(R.id.woff);
-                woff.setImageResource(R.drawable.wuff);
-                woff.setVisibility(View.VISIBLE);
-                wooof.start();
+                videoview.start();
                 woof.setOnClickListener(pausewoof);
 
 
@@ -70,9 +88,8 @@ public class MainActivity extends AppCompatActivity {
 
         woof.setOnClickListener(wooflistener);
 
-    }
 
-    
+    }
 
 
 }
